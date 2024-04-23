67c4a4d02f836340f3ec72b28d3895a618b8e7a9
jungeun919 <renee8369@likelion.org>
2024-04-23 14:22:15 +0900

    [#5] fix: replace json file data resource with Open API

--- detection/barcode_routes.py
+++ detection/barcode_routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, Response
+import requests
 import cv2
 import pyzbar.pyzbar as pyzbar
 from ..extension import socketio
@@ -19,7 +20,7 @@ def scan_barcode():
             if my_code:
                 cap.release()
                 cv2.destroyAllWindows()
-                socketio.emit('barcode', {'code': my_code})
+                get_and_save_food_info(my_code)
         
         ret, buffer = cv2.imencode('.jpg', frame)
         byte_image = buffer.tobytes()
@@ -31,6 +32,44 @@ def scan_barcode():
 @barcode.route('/video_feed')
 def video_feed():
     return Response(scan_barcode(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def get_and_save_food_info(my_code):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+        "Content-Type": "application/json"
+    }
+
+    food_response = requests.get(f"https://world.openfoodfacts.org/api/v0/product/{my_code}", headers=headers)
+    food_response.raise_for_status()
+    food_data = food_response.json()
+
+    if food_data['status'] == 1:
+        product = food_data['product']
+
+        # 무게 정보 처리
+        weight = product.get('quantity')
+        if weight:
+            weight = int(weight.split(' ')[0])
+        else:
+            weight = None
+
+        food_info = {
+            "barcode": my_code,
+            "name": product.get('product_name'),
+            "weight": weight,
+            "calories": product.get('nutriments', {}).get('energy-kcal', 0),
+            "sodium": product.get('nutriments', {}).get('sodium', 0),
+            "carbohydrate": product.get('nutriments', {}).get('carbohydrates', 0),
+            "fat": product.get('nutriments', {}).get('fat', 0),
+            "cholesterol": product.get('nutriments', {}).get('cholesterol', 0),
+            "protein": product.get('nutriments', {}).get('proteins', 0)
+        }
+
+        socketio.emit('barcode', {'food_info': food_info})
+
+    else:
+        print("Product not found")
 
 
 @barcode.route('/barcode')
--- record/routes.py
+++ record/routes.py
@@ -1,4 +1,4 @@
-from flask import Response
+from flask import Response, request
 from flask_login import current_user
 from datetime import datetime
 import json
@@ -8,37 +8,34 @@ from ..user.routes import record as day_record
 from ..extension import db
 from . import record
 
-@record.route('/barcode_record/<my_code>')
-def barcode_record(my_code):
-    with open('./static/food.json') as f:
-        food_data = json.load(f)
+@record.route('/barcode_record', methods=['POST'])
+def barcode_record():
+    data = request.get_json()
 
     new_record = Record(user_id=current_user.id, date=datetime.now())
     db.session.add(new_record)
     db.session.commit()
+
+    new_food = Food(record_id=new_record.id,
+                    name=data['name'],
+                    weight=data['weight'],
+                    calories=data['calories'], 
+                    sodium=data['sodium'], 
+                    carbohydrate=data['carbohydrate'], 
+                    fat=data['fat'], 
+                    cholesterol=data['cholesterol'],
+                    protein=data['protein'])
     
-    for i in food_data['food']:
-        if i['barcode'] == my_code:
-            new_food = Food(record_id=new_record.id,
-                            name=i['name'],
-                            weight=i['weight'],
-                            calories=i['calories'], 
-                            sodium=i['sodium'], 
-                            carbohydrate=i['carbohydrate'], 
-                            fat=i['fat'], 
-                            cholesterol=i['cholesterol'],
-                            protein=i['protein'])
+    db.session.add(new_food)
+    db.session.commit()
 
-            db.session.add(new_food)
-            db.session.commit()
-
-            t_record = Record.query.order_by(Record.id.desc()).first()
-            t_record.t_calories += i['calories']
-            t_record.t_sodium += i['sodium']
-            t_record.t_carbohydrate += i['carbohydrate']
-            t_record.t_fat += i['fat']
-            t_record.t_cholesterol += i['cholesterol']
-            t_record.t_protein += i['protein']
-            db.session.commit()
+    t_record = Record.query.order_by(Record.id.desc()).first()
+    t_record.t_calories += new_food.calories
+    t_record.t_sodium += new_food.sodium
+    t_record.t_carbohydrate += new_food.carbohydrate
+    t_record.t_fat += new_food.fat
+    t_record.t_cholesterol += new_food.cholesterol
+    t_record.t_protein += new_food.protein
+    db.session.commit()
 
     return Response(day_record())
