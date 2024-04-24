06d1a8eb9a1afcdf9b86472afb9b8db459f6c260
jisoolee11 <jisu6414@gmail.com>
2024-04-24 15:22:43 +0900

    [#2] feat: 음식 영양정보 api 연결

--- food/views.py
+++ food/views.py
@@ -10,19 +10,9 @@ import numpy as np
 import time
 from ultralytics import YOLO
 import pandas
-
-# configuration_path = "./cfg/yolov3.cfg"
-# weights_path = YOLO('yolov8n.pt')
+import requests
 
 labels = open("./data/coco.names").read().strip().split('\n')
-
-# probability_minimum = 0.5
-# threshold = 0.3
-# network = cv2.dnn.readNetFromDarknet(configuration_path, weights_path)
-
-# layers_names_all = network.getLayerNames()
-# layers_indexes_output = network.getUnconnectedOutLayers().flatten()
-# layers_names_output = [layers_names_all[i - 1] for i in layers_indexes_output]
 
 # COCO 데이터셋의 클래스 이름에서 특정 객체의 인덱스 찾기
 interested_classes = ['banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake']  # 관심 있는 객체 클래스
@@ -87,17 +77,36 @@ def upload():
     return render_template('home/upload.html')
 
 
-@food.route("/result", methods = ['GET','POST'])
+@food.route("/result", methods=['GET', 'POST'])
 def result():
-    global food_weight
+    global food_weight, products, filename
     if request.method == 'POST':
         food_weight = []
+        food_query = []
+        nutrition_data = {}
         for food in products:
             weight = request.form.get(food)
+            if weight:  # 무게 데이터가 있다면 쿼리 스트링 생성
+                food_query.append(f"{weight}g {food}")
             food_weight.append(weight)
-        print(food_weight)
-    return render_template('home/predict.html', products=products, user_image='images/output/' + filename, food_weight=food_weight)
+        # API 호출을 위한 쿼리 스트링 조합
+            query_string = ','.join(food_query)
+            nutrition_data[food] = get_nutrition_data(query_string)
 
+        print(nutrition_data)
+        return render_template('home/predict.html', products=products, user_image='images/output/' + filename, food_weight=food_weight, nutrition_data=nutrition_data)
+    else:
+        return render_template('home/predict.html', products=products, user_image='images/output/' + filename)
+
+def get_nutrition_data(query):
+    api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
+    api_key = 'DAL6Idg3vOt/hviG9ic1Xg==Xc3cWPLf0iuk4v6i'  # 여기에 실제 API 키를 입력하세요.
+    response = requests.get(api_url + query, headers={'X-Api-Key': api_key})
+    if response.status_code == 200:
+        return response.json()['items']
+    else:
+        print("Error:", response.status_code, response.text)
+        return []
 
 @food.route("/predict", methods=['GET', 'POST'])
 def predict():
@@ -122,28 +131,4 @@ def predict():
             return render_template('home/weights2.html', products=products, user_image='images/output/' + filename)
 
     return render_template('home/upload.html')
-        
-# @food.route("/predict", methods=['GET', 'POST'])
-# def predict():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file_path = os.path.join('./static/images/input', filename)
-#             file.save(file_path)
-
-#             img = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)  # 파일 경로에서 직접 이미지를 읽습니다
-#             if img is None:
-#                 return jsonify({"error": "Failed to read the image"}), 400
-
-#             # 이미지 처리 함수를 호출합니다
-#             confidence, label = inputdata(img)  # 수정된 inputdata 함수 호출
-
-#             with open('./foodnames.json', 'r', encoding='utf-8') as file:
-#                 food_names = json.load(file)
-
-#             food_name = list(food_names.values())[label]
-
-
-#             return jsonify({"filename": filename, "confidence": confidence, "label": label, "food_name": food_name})
             
