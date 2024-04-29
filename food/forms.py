d3092d7332406379276cef30d3345564959be254
jisoolee11 <jisu6414@gmail.com>
2024-04-29 15:02:10 +0900

    [#2] feat: 인식된 음식이 없을 경우 예외처리
    - 사용자가 직접 음식을 입력하도록함

--- food/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import SubmitField
-
-class PhotoUploadForm(FlaskForm):
-    photo = FileField('사진', validators=[FileAllowed(['jpg', 'png'], '이미지만 업로드 가능합니다.')])
-    submit = SubmitField('업로드')
